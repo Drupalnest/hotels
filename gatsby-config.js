b7c4b8b4cc97a028465d2d4858256692cdb67060
ankitclearview <ankitt@clearviewinfotech.com>
2024-02-07 12:26:50 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `hotel_site_neww`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
