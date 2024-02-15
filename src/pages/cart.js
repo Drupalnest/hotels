968ac9fda28dee14f1681424dbc7fe19699e7e5a
ankitclearview <ankitt@clearviewinfotech.com>
2024-02-15 17:47:46 +0530

    added files

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -3,11 +3,11 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
 const steps = [
   "Select master blaster campaign settings",
@@ -36,7 +36,30 @@ export default function Cart() {
               Almost done! Enter your details and complete your booking now.
             </p>
             <div className="">
-<div></div>
+              <div className="border border-gray-500">
+                <div>
+                  <img></img>
+                  <div>
+                    <span>NOPSI Hotel, New Orleans</span>
+                    <p>Downtown New Orleans</p>
+                    <span>VERY GOOD | 5-STAR HOTEL</span>
+                    <span>
+                      Fully Refundable until 11:59PM (property local time) on
+                      Feb 26
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  Fri, Mar 1, 2024 CHECK-OUT Tue, Mar 5, 2024 NIGHTS 4 ROOMS 1
+                </div>
+                <div>
+                  <h1>Deluxe Suite with King Bed</h1>
+                  <span>
+                    1 King Bed Free WIFI Hand sanitizer Room sanitizing Free
+                    Wifi Drinking water included Cable TV channels
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
           <div></div>
