81969fca7396a67ed2ce732b1d38ff7760c5c3c1
ankitclearview <ankitt@clearviewinfotech.com>
2024-02-23 17:59:45 +0530

    added files

--- src/components/SearchComponents/Search.js
+++ src/components/SearchComponents/Search.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Search = () => {
-  return <div className="container-fluid border-2"> Search bfgbhfg</div>;
+  return (
+    <div className="container mx-auto sm:p-5 md:p-8 lg:p-10 xl:p-12 border-2">
+      <div className="mb-4 sm:mb-6 md:mb-8 lg:mb-10 xl:mb-12">
+        {/* Your content for the first section */}
+      </div>
+      <div className="mb-4 sm:mb-6 md:mb-8 lg:mb-10 xl:mb-12">
+        {/* Your content for the second section */}
+      </div>
+      <div className="mb-4 sm:mb-6 md:mb-8 lg:mb-10 xl:mb-12">
+        {/* Your content for the third section */}
+      </div>
+      <div>
+        {/* Your content for the fourth section */}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
