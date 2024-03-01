0821576b35560322479d74e7825e739d6fa99531
ankitclearview <ankitt@clearviewinfotech.com>
2024-03-02 00:49:41 +0530

    added files

--- src/pages/mapview.js
+++ src/pages/mapview.js
@@ -368,13 +368,11 @@
 
 // export default MapComponent;
 
-
 // import React, { useState } from "react";
 // import { LoadScript, GoogleMap } from "@react-google-maps/api";
 // import MapIcon from "@mui/icons-material/Map";
 // import { navigate } from "gatsby";
 // import { useSelector } from "react-redux";
-
 
 // const MapComponent = () => {
 
@@ -387,7 +385,7 @@
 //   const [showMap, setShowMap] = useState(false);
 
 //   const handleButtonClick = () => {
-    
+
 //     navigate('/hotellist');
 //   };
 
@@ -423,7 +421,6 @@
 
 // export default MapComponent;
 
-
 // AIzaSyCzA00pEwAVjWLJ2tIMbNJY7tZjGfZeHWQ
 // import React, { useState } from "react";
 // import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
@@ -437,7 +434,6 @@
 //   if (!filteredHotels || filteredHotels.length === 0) {
 //     return <div>No hotel selected</div>;
 //   }
-
 
 //   // Checking if selectedHotel exists before accessing lat_lon
 //   const center = {
@@ -491,31 +487,160 @@
 
 // export default MapComponent;
 
-
 //AIzaSyCzA00pEwAVjWLJ2tIMbNJY7tZjGfZeHWQ
+// import React, { useState } from "react";
+// import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
+// import MapIcon from "@mui/icons-material/Map";
+// import { navigate } from "gatsby";
+// import { useSelector } from "react-redux";
+
+// const MapComponent = () => {
+//   const filteredHotels = useSelector((state) => state.hotel.filteredHotels);
+
+//   // Move the useState outside of the condition
+//   const [showMap, setShowMap] = useState(false);
+
+//   if (!filteredHotels || filteredHotels.length === 0) {
+//     return <div>No hotel selected</div>;
+//   }
+
+//   const center = {
+//     lat: filteredHotels[0]?.lat_lon?.lat || 0,
+//     lng: filteredHotels[0]?.lat_lon?.lon || 0,
+//   };
+
+//   const handleButtonClick = () => {
+//     navigate('/hotellist');
+//   };
+
+//   return (
+//     <div style={{ position: "relative", height: "800px" }}>
+//       <button
+//         className="border-2 hover:border-gray-500 bg-white font-bold p-2 rounded-xl text-blue-800 z-50 absolute top-32 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+//         style={{ zIndex: 1 }}
+//         onClick={handleButtonClick}
+//       >
+//         <MapIcon className="mr-2" />
+//         Classic View
+//       </button>
+//       <div
+//         className="border-2"
+//         style={{ width: "100%", height: "800px", borderRadius: "4%" }}
+//       >
+//         <LoadScript googleMapsApiKey="AIzaSyCzA00pEwAVjWLJ2tIMbNJY7tZjGfZeHWQ">
+//           <GoogleMap
+//             mapContainerStyle={{
+//               width: "100%",
+//               height: "100%",
+//               borderRadius: "4%",
+//             }}
+//             center={center}
+//             zoom={10}
+//           >
+//             {filteredHotels.map((hotel) => (
+//               <Marker
+//                 key={hotel.id}
+//                 position={{
+//                   lat: hotel.lat_lon?.lat || 0,
+//                   lng: hotel.lat_lon?.lon || 0,
+//                 }}
+//                 icon={{ url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png' }}
+//               />
+//             ))}
+//           </GoogleMap>
+//         </LoadScript>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MapComponent;
+
 import React, { useState } from "react";
-import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
+import {
+  LoadScript,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  OverlayView,
+} from "@react-google-maps/api";
 import MapIcon from "@mui/icons-material/Map";
 import { navigate } from "gatsby";
 import { useSelector } from "react-redux";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+const HotelPopup = ({ hotel, onClose }) => {
+  return (
+    <InfoWindow
+      position={{ lat: hotel.lat_lon?.lat || 0, lng: hotel.lat_lon?.lon || 0 }}
+      onCloseClick={onClose}
+    >
+      <div className="w-64 p-4 bg-white border rounded shadow-md">
+        <div className="mb-4">
+          <img
+            src={hotel.image} // Add the image source for the hotel
+            alt={hotel.name}
+            className="w-full h-32 object-cover rounded"
+          />
+        </div>
+        <div className="flex items-center justify-between mb-2">
+          <div className="flex items-center">
+            <span className="text-green-500 mr-1">
+              <FavoriteIcon />
+              {/* Replace with the actual AddLoveMaterialUIIcon component */}
+            </span>
+            <p className="text-gray-700">9.3</p>
+          </div>
+          <p className="text-gray-700">{hotel.location}</p>
+          <p className="text-gray-700">5-Star Hotel</p>
+        </div>
+        <h2 className="text-xl font-semibold mb-2">{hotel.name}</h2>
+      </div>
+    </InfoWindow>
+  );
+};
+
+const CustomMarker = ({ position, onClick }) => {
+  return (
+    <OverlayView
+      position={position}
+      mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+    >
+      <div
+        style={{
+          position: "absolute",
+          cursor: "pointer",
+          transform: "translate(-50%, -50%)",
+        }}
+        onClick={onClick}
+      >
+        <div className="p-1 px-2 bg-blue-600 flex justify-center items-center rounded-full">
+          <p className="text-sm text-white font-bold">₹25360</p>
+        </div>
+      </div>
+    </OverlayView>
+  );
+};
 
 const MapComponent = () => {
   const filteredHotels = useSelector((state) => state.hotel.filteredHotels);
 
-  // Move the useState outside of the condition
-  const [showMap, setShowMap] = useState(false);
+  const [selectedHotel, setSelectedHotel] = useState(null);
 
-  if (!filteredHotels || filteredHotels.length === 0) {
-    return <div>No hotel selected</div>;
-  }
+  const handleMarkerClick = (hotel) => {
+    setSelectedHotel(hotel);
+  };
+
+  const handleMapClick = () => {
+    setSelectedHotel(null);
+  };
+
+  const handleButtonClick = () => {
+    navigate("/hotellist");
+  };
 
   const center = {
     lat: filteredHotels[0]?.lat_lon?.lat || 0,
     lng: filteredHotels[0]?.lat_lon?.lon || 0,
-  };
-
-  const handleButtonClick = () => {
-    navigate('/hotellist');
   };
 
   return (
@@ -541,17 +666,25 @@ const MapComponent = () => {
             }}
             center={center}
             zoom={10}
+            onClick={handleMapClick}
           >
             {filteredHotels.map((hotel) => (
-              <Marker
+              <CustomMarker
                 key={hotel.id}
                 position={{
                   lat: hotel.lat_lon?.lat || 0,
                   lng: hotel.lat_lon?.lon || 0,
                 }}
-                icon={{ url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png' }}
+                onClick={() => handleMarkerClick(hotel)}
               />
             ))}
+
+            {selectedHotel && (
+              <HotelPopup
+                hotel={selectedHotel}
+                onClose={() => setSelectedHotel(null)}
+              />
+            )}
           </GoogleMap>
         </LoadScript>
       </div>
