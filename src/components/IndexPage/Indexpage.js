dfa0ad1f6039b1756e625743c74409728ffb1b54
ankitclearview <ankitt@clearviewinfotech.com>
2024-03-04 16:53:27 +0530

    added files

--- src/components/IndexPage/Indexpage.js
+++ src/components/IndexPage/Indexpage.js
@@ -16,14 +16,13 @@ const Indexpage = ({ index }) => {
   };
 
   return (
-    <div className="container border border-green-500">
-      <div className=" border border-red-500">
-        <div className="border border-red-200 p-4">
-          <p className="font-montserrat font-semibold text-2xl leading-10 text-blue-800">
-            Save big on your next hotel
-          </p>
-
-          <div className="border border-rounded-3 flex">
+    <div className="container ">
+      <div className=" ">
+        <p className="m-3 font-montserrat font-semibold text-3xl leading-10 text-blue-800">
+          Save big on your next hotel
+        </p>
+        <div className=" space-y-4 border shadow-2xl rounded-3xl p-3 ">
+          <div className=" flex">
             <div
               className={`flex p-2 ${
                 isActive
@@ -121,7 +120,7 @@ const Indexpage = ({ index }) => {
               </div>
             </div>
           </div>
-          <div className="border rounded-lg">
+          <div className="sm:w-full border rounded-lg">
             <TextField
               placeholder="Where to?"
               InputProps={{
@@ -135,72 +134,75 @@ const Indexpage = ({ index }) => {
             />
           </div>
 
-          <div>
-            <div className="flex justify-between gap-2 ">
-              <div className="rounded-xl flex items-center border-2 p-1 w-1/2">
-                <div className="mr-2">
-                  <CalendarMonthIcon className="font-montserrat font-bold text-2xl leading-10 text-blue-800" />
+          <div className="flex flex-col sm:flex-row  justify-betwee text-blue-800 gap-3 ">
+            <div className=" rounded-xl flex items-center border-2 p-2 w-full sm:w-1/2">
+              <div className="mr-2">
+                <CalendarMonthIcon className="font-montserrat font-bold text-2xl leading-10 text-blue-800" />
+              </div>
+              <div className="flex flex-col">
+                <div>
+                  <p className="text-sm font-bold">Check-in - Check-out</p>
                 </div>
-                <div className="flex flex-col">
-                  <div>
-                    <p>Check-in - Check-out</p>
-                  </div>
-                  <div>
-                    <p>03/04/2024 - 03/05/2024 </p>
-                  </div>
+                <div>
+                  <p>03/04/2024 - 03/05/2024 </p>
                 </div>
               </div>
-
-              <button className="rounded-xl flex items-center border-2 p-1 w-1/2">
-                <div className="flex items-center">
-                  <div>
-                    <PersonIcon className="font-montserrat font-bold text-2xl leading-10 text-blue-800" />
-                  </div>
-                  <div className="mr-2">
-                    <p>2 Adults, 1 Room</p>
-                  </div>
-                </div>
-              </button>
             </div>
 
-            <div className="flex w-full  gap-2">
-              <div className="rounded-xl p-1 flex items-center border-2 w-1/2">
+            <button className=" rounded-xl flex items-center border-2 p-2 w-full sm:w-1/2">
+              <div className="flex items-center">
                 <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    height="24"
-                    width="24"
-                    aria-hidden="true"
-                    fill="currentcolor"
-                    color="highlight.shade"
-                    tabindex="-1"
-                    focusable="false"
-                    role="img"
-                    class="Svg-sc-12lgb6u-0 bkdMKt Discount__SvgDiscount-sc-1s5abed-0 fkoOZw"
-                  >
-                    <path d="M23 12l-2.4 2.7.3 3.5-3.6.8-1.9 3-3.4-1.4L8.6 22l-1.9-3-3.6-.8.3-3.5L1 12l2.4-2.7-.3-3.5L6.7 5l1.9-3L12 3.4 15.4 2l1.9 3 3.6.8-.3 3.5L23 12zm-10.8-.6c-1.3-.3-1.8-.7-1.8-1.3 0-.6.6-1.1 1.6-1.1s1.4.5 1.5 1.2h1.3c0-1-.7-1.9-1.9-2.2V6.7h-1.8V8c-1.1.2-2 1-2 2.1 0 1.3 1.1 2 2.8 2.4 1.5.4 1.8.9 1.8 1.4 0 .4-.3 1-1.6 1-1.2 0-1.7-.5-1.8-1.2H9c.1 1.3 1 2 2.2 2.2v1.3H13V16c1.1-.2 2.1-.9 2.1-2.1-.1-1.6-1.5-2.2-2.9-2.5z"></path>
-                  </svg>
+                  <PersonIcon className="font-montserrat font-bold text-2xl leading-10 text-blue-800" />
                 </div>
-                <div className="flex flex-row">
-                  <div>
-                    <p>Bundle + save</p>
-                  </div>
-                  <div className="flex ">
-                    <CheckBox />
-                    <p>Add a car</p>
-                    <CheckBox />
-                    <p>Add a flight</p>
-                  </div>
+                <div className="">
+                  <p>2 Adults, 1 Room</p>
                 </div>
               </div>
+            </button>
+          </div>
 
-              <button className=" rounded-xl p-1 items-center border-2 w-1/2">
-                <div className="flex items-center">
-                  <p>Find Your Hotel</p>
+          <div className="flex flex-col sm:flex-row w-full text-green-800 gap-3">
+            <div className="bg-green-100 rounded-xl p-2 flex items-center border-2 w-full sm:w-1/2 overflow-x-auto">
+              <div className="mr-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  height="24"
+                  width="24"
+                  aria-hidden="true"
+                  fill="currentcolor"
+                  color="highlight.shade"
+                  tabIndex="-1"
+                  focusable="false"
+                  role="img"
+                  className="Svg-sc-12lgb6u-0 bkdMKt Discount__SvgDiscount-sc-1s5abed-0 fkoOZw"
+                >
+                  <path d="M23 12l-2.4 2.7.3 3.5-3.6.8-1.9 3-3.4-1.4L8.6 22l-1.9-3-3.6-.8.3-3.5L1 12l2.4-2.7-.3-3.5L6.7 5l1.9-3L12 3.4 15.4 2l1.9 3 3.6.8-.3 3.5L23 12zm-10.8-.6c-1.3-.3-1.8-.7-1.8-1.3 0-.6.6-1.1 1.6-1.1s1.4.5 1.5 1.2h1.3c0-1-.7-1.9-1.9-2.2V6.7h-1.8V8c-1.1.2-2 1-2 2.1 0 1.3 1.1 2 2.8 2.4 1.5.4 1.8.9 1.8 1.4 0 .4-.3 1-1.6 1-1.2 0-1.7-.5-1.8-1.2H9c.1 1.3 1 2 2.2 2.2v1.3H13V16c1.1-.2 2.1-.9 2.1-2.1-.1-1.6-1.5-2.2-2.9-2.5z"></path>
+                </svg>
+              </div>
+              <div className="flex p-1 py-2 flex-row text-sm md:text-base font-semibold">
+                <div>
+                  <p>Bundle + save</p>
                 </div>
-              </button>
+                <div className="flex items-center ml-2">
+                  <CheckBox className="text-lg" />
+                  <p className="ml-1">Add a car</p>
+                  {/* <CheckBox className="ml-2" />
+                  <p className="ml-1">Add a flight</p> */}
+                </div>
+              </div>
             </div>
+
+            <button className="bg-blue-800 rounded-xl p-2 items-center border-2 w-full sm:w-1/2">
+              <div className=" text-blue-800 font-bold flex items-center justify-center">
+                <p className="text-white">Find Your Hotel</p>
+              </div>
+            </button>
+          </div>
+          <div className="border-2 w-full sm:w-1/2 md:w-1/2  lg:w-1/2flex lg:justify-end ">
+            <p className="text-xs">
+              Book a hotel with free cancellation for flexibility
+            </p>
           </div>
 
           <div>
